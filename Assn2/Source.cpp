23c863c265729bc0b57d1934c29ee0adff42fa45
Cyril <megarediron@gmail.com>
2022-09-20 13:56:03 -0400

    Completed Step 3

--- Assn2/Source.cpp
+++ Assn2/Source.cpp
@@ -1,3 +1,34 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+using namespace std;
+
+struct STUDENT_DATA {             // Structure declaration
+    string firstName;         // Member (string variable)
+    string lastName;   // Member (string variable)
+};       // Structure variable
+
 int main() {
-	return 1;
+    vector <STUDENT_DATA> collected;
+    string line;
+    ifstream myfile("StudentData.txt");
+
+    while (getline(myfile, line))
+    {
+        stringstream ss(line);
+
+        STUDENT_DATA temp;
+
+        string fname, lname;
+        getline(ss, fname, ',');
+        temp.firstName = fname;
+        getline(ss, lname, ',');
+        temp.lastName = lname;
+        collected.push_back(temp);
+    }
+
 }
