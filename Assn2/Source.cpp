6db9411fca38b1feee41b1fe6458cbffd19d7b7b
Cyril <megarediron@gmail.com>
2022-09-21 00:33:30 -0400

    Step 5.3

--- Assn2/Source.cpp
+++ Assn2/Source.cpp
@@ -7,15 +7,30 @@
 #include <sstream>
 using namespace std;
 
+#define PRERELEASE
+
 struct STUDENT_DATA {             // Structure declaration
     string firstName;         // Member (string variable)
     string lastName;   // Member (string variable)
+
+#ifdef PRERELEASE
+    string email;
+#endif // PRERELEASE
+
+
 };       // Structure variable
 
+
 int main() {
+#ifdef PRERELEASE
+    cout << "Regular code running -- PreRelease Version" << endl;
+    ifstream myfile("StudentData_Emails.txt");
+#else
+    cout << "Regular code running" << endl;
+    ifstream myfile("StudentData.txt");
+#endif // 
     vector <STUDENT_DATA> collected;
     string line;
-    ifstream myfile("StudentData.txt");
 
     while (getline(myfile, line))
     {
@@ -23,11 +38,16 @@ int main() {
 
         STUDENT_DATA temp;
 
-        string fname, lname;
+        string fname, lname, email;
         getline(ss, fname, ',');
         temp.firstName = fname;
         getline(ss, lname, ',');
         temp.lastName = lname;
+#ifdef PRERELEASE
+        getline(ss, email, ',');
+        temp.email = email;
+#endif // PRERELEASE
+
         collected.push_back(temp);
     }
 #ifdef _DEBUG
